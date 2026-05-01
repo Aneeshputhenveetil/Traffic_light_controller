f7b88419373dae4b42bb84d5ffbb8cd67ddc86cb
aneesh_R <aneeshects@gmail.com>
2026-05-01 17:00:32 +0530

    basic scheduler created with timer and blink 3 different led in 3 different time

--- Traffic_light/Traffic_light.ino
+++ Traffic_light/Traffic_light.ino
@@ -21,37 +21,87 @@
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
+#define LED_RED 14
+#define LED_YELLOW 15
+#define LED_GREEN 16
+
 #include "Timer.h"
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
   
   Timer_Init();
 }
 
-static uint32_t led_count = 0;
+
 static uint32_t current_time = 0;
+
+static uint32_t led_count = 0;
 static uint8_t led_flag = 0;
+
+static uint32_t led_count1 = 0;
+static uint8_t led_flag1 = 0;
+
+static uint32_t led_count2 = 0;
+static uint8_t led_flag2 = 0;
+
+static uint32_t led_count3 = 0;
+static uint8_t led_flag3 = 0;
 
 
 // the loop function runs over and over again forever
 void loop() {
   current_time = Timer_Get_Millis();
-
-  if((current_time - led_count) >= 100)
+  
+  if((current_time - led_count1) >= 100)
   {
-    led_count = current_time;
-    if(led_flag)
+    led_count1 = current_time;
+    if(led_flag1)
     {
-      led_flag = 0;
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      led_flag1 = 0;
+      digitalWrite(LED_RED, HIGH);   // turn the LED on (HIGH is the voltage level)
     }
     else
     {
-      led_flag = 1;
-      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      led_flag1 = 1;
+      digitalWrite(LED_RED, LOW);    // turn the LED off by making the voltage LOW
     }
   }
 
+  if((current_time - led_count2) >= 100)
+  {
+    led_count2 = current_time;
+    if(led_flag2)
+    {
+      led_flag2 = 0;
+      digitalWrite(LED_YELLOW, HIGH);   // turn the LED on (HIGH is the voltage level)
+    }
+    else
+    {
+      led_flag2 = 1;
+      digitalWrite(LED_YELLOW, LOW);    // turn the LED off by making the voltage LOW
+    }
+  }
+
+
+    if((current_time - led_count3) >= 500)
+  {
+    led_count3 = current_time;
+    if(led_flag3)
+    {
+      led_flag3 = 0;
+      digitalWrite(LED_GREEN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    }
+    else
+    {
+      led_flag3 = 1;
+      digitalWrite(LED_GREEN, LOW);    // turn the LED off by making the voltage LOW
+    }
+  }
+
+  
 }
